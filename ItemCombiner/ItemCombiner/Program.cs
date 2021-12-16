59f9c86bc4e5ca1a54b6161f2f95611127af6dda
ColumFreytag <96263394+ColumFreytag@users.noreply.github.com>
2021-12-16 13:49:33 -0800

    Add files via upload

---
+++ ItemCombiner/ItemCombiner/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace ItemCombiner
+{
+    class Program
+    {
+        
+
+        static void Main()
+        {
+            WriteLine("start");
+            ReadKey(true);
+            Deposit();
+            
+        }
+
+        static List<string> Inventory = new List<string>();
+        static string input = " ";
+        static void Deposit()
+        {
+            Inventory.Add("Banana");
+            Inventory.Add("Knife");
+            Inventory.Add("Banana");
+            Inventory.Add("Knife");
+            Inventory.Add("Banana");
+            Inventory.Add("Knife");
+            Inventory.Add("Banana");
+            Inventory.Add("Knife");
+
+            ListInventory();
+
+            while (Inventory.Contains("Knife") && Inventory.Contains("Banana"))
+            {
+                WriteLine("Do you want to cut this banana?");
+                input = ReadLine().ToLower();
+
+                if(input.StartsWith("y"))
+                {
+                    Inventory.Remove("Banana");
+                    Inventory.Remove("Knife");
+                    Inventory.Add("Sliced Banana");
+                }
+                else
+                {
+                    WriteLine("You spared the banana");
+                    break;
+                }
+                
+            }
+
+
+            ListInventory();
+            ReadKey(true);
+        }
+
+        static void ListInventory()
+        {
+            WriteLine("You now posses");
+            foreach (var item in Inventory)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
